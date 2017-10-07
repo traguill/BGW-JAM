37bc160042e067e8e7c03d6e013aa8c5c0d70ba8
traguill <traguill1@gmail.com>
2017-10-07 07:06:17 +0200

    Menu remastered

--- JamProject/Assets/Scripts/MenuManager.cs
+++ JamProject/Assets/Scripts/MenuManager.cs
@@ -7,39 +7,42 @@ using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour {
 
-    public GameObject start_focus;
-
-    bool controls_visible = false;
-
     public string game_scene = "GameScene";
     public string credits_scene = "CredtisScene";
 
-    public Image controls_img;
+    public Image play;
+    public Image credits;
+    public Image exit;
+
+    public Image arrow;
+
+    GameObject current_selected;
+
+    public Color selected_color;
+    public Color unselected_color;
 
 	// Use this for initialization
 	void Start () {
-		EventSystem.current.SetSelectedGameObject(start_focus);
+		EventSystem.current.SetSelectedGameObject(play.gameObject);
+
+        current_selected = play.gameObject;
+        HandleSelectionAnim();
 	}
+
+    void Update()
+    {
+        GameObject selected = EventSystem.current.currentSelectedGameObject;
+        if(selected != current_selected)
+        {
+            arrow.rectTransform.anchoredPosition = new Vector2(arrow.rectTransform.anchoredPosition.x, selected.transform.localPosition.y);
+            current_selected = selected;
+            HandleSelectionAnim();
+        }
+    }
 	
 	public void PlayPressed()
     {
         SceneManager.LoadScene(game_scene, LoadSceneMode.Single);
-    }
-
-    public void ControlsPressed()
-    {
-        controls_visible = !controls_visible;
-
-        if(controls_visible)
-        {
-            //Show hide controls
-            controls_img.enabled = true;
-        }
-        else
-        {
-            //Show hide controls
-            controls_img.enabled = false;
-        }
     }
 
     public void CreditsPressed()
@@ -51,4 +54,43 @@ public class MenuManager : MonoBehaviour {
     {
         Application.Quit();
     }
+
+    void HandleSelectionAnim()
+    {
+
+        if(current_selected == play.gameObject)
+        {
+            play.color = selected_color;
+            play.rectTransform.anchoredPosition = new Vector2(494, play.rectTransform.anchoredPosition.y);
+        }
+        else
+        {
+            play.color = unselected_color;
+            play.rectTransform.anchoredPosition = new Vector2(514, play.rectTransform.anchoredPosition.y);
+        }
+
+        if (current_selected == credits.gameObject)
+        {
+            credits.color = selected_color;
+            credits.rectTransform.anchoredPosition = new Vector2(554, credits.rectTransform.anchoredPosition.y);
+        }
+        else
+        {
+            credits.color = unselected_color;
+            credits.rectTransform.anchoredPosition = new Vector2(574, credits.rectTransform.anchoredPosition.y);
+        }
+
+        if (current_selected == exit.gameObject)
+        {
+            exit.color = selected_color;
+            exit.rectTransform.anchoredPosition = new Vector2(491, exit.rectTransform.anchoredPosition.y);
+        }
+        else
+        {
+            exit.color = unselected_color;
+            exit.rectTransform.anchoredPosition = new Vector2(511, exit.rectTransform.anchoredPosition.y);
+        }
+    }
+
+
 }
