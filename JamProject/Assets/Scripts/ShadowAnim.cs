f0d1698f0d30381d0de292605a2f81225a104e27
traguill <traguill1@gmail.com>
2017-10-07 14:38:48 +0200

    Shadows

---
+++ JamProject/Assets/Scripts/ShadowAnim.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadowAnim : MonoBehaviour {
+
+    public float speed = 2.0f;
+
+    public float displacement = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float x_val = displacement * Mathf.Sin(Time.timeSinceLevelLoad * speed);
+
+        transform.position = new Vector3(x_val, transform.position.y, transform.position.z);
+
+	}
+}
