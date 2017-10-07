69531951b2c90e26fb3586009dee14790fa4812c
Ausiàs Dalmau <ausi_dalmau@hotmail.com>
2017-10-07 11:37:03 +0200

    BOunds

---
+++ JamProject/Assets/RenderOnFather.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderOnFather : MonoBehaviour {
+
+    SpriteRenderer s_ren;
+    SpriteRenderer p_ren;
+	// Use this for initialization
+	void Start () {
+        s_ren = GetComponent<SpriteRenderer>();
+        p_ren = transform.parent.GetComponent<SpriteRenderer>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        s_ren.sortingOrder = p_ren.sortingOrder + 1;
+	}
+}
--- JamProject/Assets/Scripts/DeathCollider.cs
+++ JamProject/Assets/Scripts/DeathCollider.cs
@@ -6,7 +6,12 @@ public class DeathCollider : MonoBehaviour
 {
     public string bullet_tag;
     public Player player;
-	
+
+    float x=-184;
+    float y= 96;
+    float w=179;
+    float h=-112;
+
     void OnTriggerEnter2D(Collider2D col)
     {
         if(col.tag == bullet_tag)
@@ -14,5 +19,17 @@ public class DeathCollider : MonoBehaviour
             Debug.Log("Player: "+player.player_id+" has been hitted");
             player.Hit(col.gameObject.GetComponent<Bullet>());
         }
+    }
+
+    private void Update()
+    {
+        if (transform.parent.position.x < x)
+            transform.parent.position = new Vector3(x, transform.parent.position.y);
+        if (transform.parent.position.y > y)
+            transform.parent.position = new Vector3(transform.parent.position.x, y);
+        if (transform.parent.position.x > w)
+            transform.parent.position = new Vector3(w, transform.parent.position.y);
+        if (transform.parent.position.y < h)
+            transform.parent.position = new Vector3(transform.parent.position.x, h);
     }
 }
